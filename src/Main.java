805d454b63651ed83ac147c1fdb5f3d321ae83ac
Sergey <vimpel812@gmail.com>
2022-09-15 22:00:47 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+        var dog = 8;
+        var cat = 3.6;
+        var paper = 763789;
+
+        System.out.println(dog);
+        System.out.println(cat);
+        System.out.println(paper);
+
+        dog+=4;
+        cat+=4;
+        paper+=4;
+
+        System.out.println(dog);
+        System.out.println(cat);
+        System.out.println(paper);
+    }
+}
